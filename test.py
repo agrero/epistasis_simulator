38639b9b0545b2f7dce8c5640636ea24e5ea2389
agrero <86486604+agrero@users.noreply.github.com>
2022-08-02 12:55:53 -0700

    Better compartamentalized the different modules for readabilities sake created the sire class which is the primogenitor for all subsequent mutations imported all energy functions from the jupyter notebook added some mutation functions in gene_functions as they aren't necessarily fit for methods

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print('hello wold')
+import epistasiscomponents
+from epistasiscomponents import constants
+from epistasiscomponents import primogenitor as pg
+
+parent = pg.Primogenitor("AAAAAAAAAAAAAAAA", 'S100 test')
+
+print(parent)
+
