58aefaf225e2c9cd2cc17dfff8b6e89cc9b66cef
agrero <86486604+agrero@users.noreply.github.com>
2022-08-03 10:23:24 -0700

    the mutation method for sire now will randomly assign new mutations skipping over already mutated acids

--- epistasiscomponents/epistasis_objects/sire.py
+++ epistasiscomponents/epistasis_objects/sire.py
@@ -3,6 +3,7 @@ import random
 import epistasiscomponents
 #make this more concise later
 from epistasiscomponents.epistasis_functions.gene_functions import create_gene_ndxs, mutate
+from epistasiscomponents.constants import AMINO_ACIDS
 
 class Sire:
     """
@@ -12,6 +13,7 @@ class Sire:
 
         self.sequence = sequence
         self.name = name
+        self.deleterious_mutants = []
 
     #in the future may want to add progeny size as a parameter
     def create_mutants(self, no_mutations):
@@ -28,13 +30,23 @@ class Sire:
 
         mutants_list = []
 
+        if len(self.sequence) < no_mutations:
+            return []
+
         count = 0
         while count != no_mutations:
             if len(mutants_list) != 0:
                 seq_to_mutate = mutants_list[-1]
+
+            #insert energy check here for each mutant, returning to the start
+            #if the conditions are not met
+            #add deleterious mutations to the self.deleterious_mutants
+
+            mutant_acid = random.choice(AMINO_ACIDS)
             mutant_ndx = random.choice(gene_ndxs)
             gene_ndxs.remove(mutant_ndx)
-            mutation = (mutant_ndx, 'G')
+
+            mutation = (mutant_ndx, mutant_acid)
             mutant_sequence = mutate(seq_to_mutate, mutation)
             mutants_list.append(mutant_sequence)
             count += 1
