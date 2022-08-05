a074f2e6741bc4556677f081f2d75706b76f2212
agrero <86486604+agrero@users.noreply.github.com>
2022-08-05 11:52:43 -0700

    deleted test

--- test.py
+++
@@ -1,8 +0,0 @@
-import epistasiscomponents
-from epistasiscomponents.epistasis_objects import sire 
-from epistasiscomponents.epistasis_functions import gene_functions as gf
-
-parent = sire.Sire("AAAAAAAAA", 'S100 test')
-
-print(parent.create_mutants(3))
-
