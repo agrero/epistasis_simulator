801c5f1b3eb3e8c875bc75f632daa83f78b1948e
agrero <86486604+agrero@users.noreply.github.com>
2022-08-12 12:55:16 -0700

    reconfigured mutations to make the output a panda dataframe

--- epistasiscomponents/epistasis_objects/sire.py
+++ epistasiscomponents/epistasis_objects/sire.py
@@ -1,11 +1,13 @@
 import random
 
-import epistasiscomponents
 #make this more concise later
-from epistasiscomponents.epistasis_functions.gene_functions import create_gene_ndxs, mutate
-from epistasiscomponents.constants import AMINO_ACIDS
+from epistasiscomponents.epistasis_functions.gene_functions import create_gene_ndxs
+from epistasiscomponents.epistasis_functions.constants import AMINO_ACIDS, DDG
+from epistasiscomponents.epistasis_functions.energy_functions import dg_obs
 
 class Sire:
+
+    #this is also innaccurate
     """
     The parent gene that will be undergoing mutations in order to create its scion
     """
@@ -13,10 +15,12 @@ class Sire:
 
         self.sequence = sequence
         self.name = name
-        self.deleterious_mutants = []
+        self.potential_progeny = []
+        self.wt_nrg = dg_obs()
 
     #in the future may want to add progeny size as a parameter
-    def create_mutants(self, no_mutations):
+    def create_mutant(self , no_mutations):
+        #rewrite this to how this actually works now
         """
         Generates a list of indices based on the length of the given sequence.
         The function then randomly picks an amino acid to be mutated, removing the index from the list.
@@ -24,35 +28,47 @@ class Sire:
         and the values are derived from the specific mutations that occured to create the 
         mutant.
         """
-        mutable_range = range(len(self.sequence))
-        gene_ndxs = create_gene_ndxs(mutable_range)
-        seq_to_mutate = self.sequence
-
-        mutants_list = []
 
         if len(self.sequence) < no_mutations:
-            return []
+            raise Exception("You cannot have more mutations than amino acids")
 
-        count = 0
-        while count != no_mutations:
-            if len(mutants_list) != 0:
-                seq_to_mutate = mutants_list[-1]
+        mutable_range = range(len(self.sequence))
+        gene_ndxs = create_gene_ndxs(mutable_range)
 
-            #insert energy check here for each mutant, returning to the start
-            #if the conditions are not met
-            #add deleterious mutations to the self.deleterious_mutants
+        mutant = []
 
-            mutant_acid = random.choice(AMINO_ACIDS)
-            mutant_ndx = random.choice(gene_ndxs)
-            gene_ndxs.remove(mutant_ndx)
+        for mutation in range(no_mutations): #theres that enumerate thing that hsould go here
 
-            mutation = (mutant_ndx, mutant_acid)
-            mutant_sequence = mutate(seq_to_mutate, mutation)
-            mutants_list.append(mutant_sequence)
-            count += 1
+                mutant_acid = random.choice(AMINO_ACIDS)
+                mutant_ndx = random.choice(gene_ndxs)
+                gene_ndxs.remove(mutant_ndx)
 
-        return mutants_list
+                mutation = f"{self.sequence[mutant_ndx]}{mutant_ndx+1}{mutant_acid}" 
+                mutant.append(mutation)
+        
+        self.potential_progeny.append(mutant)
+
+    def mutant_energies(self):
+        """
+        Returns a list of mutation energies as derived from the ddg.csv. The mutations are 
+        taken from the self.potential_progeny.
+        """
+
+        if len(self.potential_progeny) == 0:
+            raise Exception("You cannot get a mutant energy without any mutants.\n Call .create_mutant first.")
+
+        energies = []
+
+        for mutant in self.potential_progeny:
+            energies.append(DDG.loc[mutant])
+            energies.append(mutant.sum())
+        
+
+
+        return energies
+
     def __str__(self) -> str:
-        return """Name: {}
+        return """
+Name: {}
 Sequence: {}
         """.format(self.name, self.sequence)
